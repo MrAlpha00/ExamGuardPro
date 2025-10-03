54d9544502f489af70e07c4dc84ec4ce042af3ca
Mr Alpha <147964466+MrAlpha00@users.noreply.github.com>
2025-10-03 17:35:08 +0000

    email change

--- server/storage.ts
+++ server/storage.ts
@@ -84,13 +84,14 @@ export class DatabaseStorage implements IStorage {
       .insert(users)
       .values(userData)
       .onConflictDoUpdate({
-        target: users.email,
+        target: users.email, // 👈 use email instead of id
         set: {
           ...userData,
           updatedAt: new Date(),
         },
       })
       .returning();
+
     return user;
   }
 
