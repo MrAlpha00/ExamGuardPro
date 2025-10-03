d346a51342d449c7b5284b14f07fcc53959af0e6
Mr Alpha <147964466+MrAlpha00@users.noreply.github.com>
2025-10-03 15:52:38 +0000

    size of file extended

--- server/index.ts
+++ server/index.ts
@@ -9,8 +9,9 @@ if (process.env.NODE_ENV === "production") {
   app.set("trust proxy", 1);
 }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// allow larger payloads (up to 50mb)
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
